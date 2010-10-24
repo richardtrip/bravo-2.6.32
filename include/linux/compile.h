d973172d63cab5d08d674f343364b6d8be7f319a
Richard Trip <richard.trip@gmail.com>
2010-10-24 20:15:56 +0200

    Add missing files

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 14 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#14 PREEMPT Tue Oct 19 20:03:01 CEST 2010"
+#define LINUX_COMPILE_TIME "20:03:01"
+#define LINUX_COMPILE_BY "trip"
+#define LINUX_COMPILE_HOST "trip-laptop"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
